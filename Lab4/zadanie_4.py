7fb9b5a6dd9bd4e6319093b0271e34fb5f2a798a
Kacper <154221236+KacSta@users.noreply.github.com>
2026-04-06 19:57:12 +0200

    Lab 4

---
+++ Lab4/zadanie_4.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+app = FastAPI()
+X = np.array([[10], [20], [30], [40], [50]])
+y = np.array([21, 41, 61, 81, 101])
+
+model = LinearRegression()
+model.fit(X, y)
+
+class PredictRequest(BaseModel):
+    feature: float
+
+@app.get("/")
+def read_root():
+    return {"message": "API ML gotowe do pracy. Sprawdź /docs po instrukcję."}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok", "uptime": "stable"}
+
+@app.get("/info")
+def get_model_info():
+    return {
+        "model_type": type(model).__name__,
+        "n_features_in": model.n_features_in_,
+        "coefficients": model.coef_.tolist(),
+        "intercept": float(model.intercept_),
+        "decsription": "Model regresji liniowej przewidujący y = 2x + 1"
+    }
+
+
+@app.post("/predict")
+def predict(request: PredictRequest):
+    if request.feature < 0:
+        raise HTTPException(
+            status_code=400,
+            detail="Wartość cechy nie może być ujemna. Podaj liczbę dodatnią."
+        )
+    try:
+        prediction = model.predict([[request.feature]])
+        return {
+            "input": request.feature,
+            "prediction": float(prediction[0])
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500,
+        detail=f"Błąd modelu: {str(e)}")
+
+    # irm "http://127.0.0.1:8000/health"
+    # irm "http://127.0.0.1:8000/info"
